4b7dcbe705f8626803e9a60d8b6d0a9aa0be0168
Benjamin Sternthal <ben@devpatch.com>
2017-08-13 13:32:41 -0700

    testing

--- app.js
+++ app.js
@@ -2,40 +2,30 @@
 
 require('dotenv').config();
 
-const Hat = require('./lib/sensorHatClient');
-// const Influx = require('./lib/influx');
+const hat = require('./lib/sensorHatClient');
+const Influx = require('./lib/influx');
 const SlackBot = require('slackbots');
 
-const Delay = process.env.UPDATE_FREQUENCY
+const Delay = process.env.UPDATE_FREQUENCY;
 
 // Create & Configure Slackbot
-// let bot = new SlackBot({
-//     token: process.env.SLACK_API_TOKEN,
-//     name: process.env.SLACK_BOT_NAME,
-// });
-// let channel = process.env.SLACK_CHANNEL;
-// let params = {icon_emoji: ':tfws:'};
+let bot = new SlackBot({
+    token: process.env.SLACK_API_TOKEN,
+    name: process.env.SLACK_BOT_NAME,
+});
+let channel = process.env.SLACK_CHANNEL;
+let params = {icon_emoji: ':tfws:'};
 
-//bot.postMessageToGroup(channel, 'Living Room Has Started', params);
-
-// function getData()
-//     Hat.getHatData().then(Influx.writeInflux).then(function() {
-//         setTimeout(getData, Delay);
-//     }).catch(function(e) {
-//         bot.postMessageToGroup(channel,  e.message);
-//         // Retry
-//         setTimeout(getData, Delay);
-//     });
-//
-// };
-//
+bot.postMessageToGroup(channel, 'Living Room Has Started', params);
 
 function getData() {
-    Hat.getSenseHatJSON().then(function(data) {
-        console.log(data);
+    Hat.getHatData().then(Influx.writeInflux).then(function() {
+        setTimeout(getData, Delay);
     }).catch(function(e) {
-        console.log(e);
+        bot.postMessageToGroup(channel, e.message);
+        // Retry
+        setTimeout(getData, Delay);
     });
-}
+};
 
 getData();
